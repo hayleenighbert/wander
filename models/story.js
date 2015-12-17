a1c87d4e8464eb907da93d799002243f2f53020c
Haylee Nighbert <haylee.nigh@gmail.com>
2015-12-17 13:09:56 -0800

    Form

--- models/story.js
+++ models/story.js
@@ -4,7 +4,7 @@ var StorySchema = new mongoose.Schema({
   title: String,
   location: String,
   description: String,
-  image: String,
+  imageUploadUrl: String,
   user_id: {type: String, ref: 'User'}
 });
 
